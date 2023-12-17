6b498b4dc84bcd69233472eae188580264b3417f
Madhav Anand <madhavanand402@gmail.com>
2023-12-17 08:31:03 +0530

    added functions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
 import { MinusIcon, PlusIcon, ResetIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export default function Card() {
+  const [count, setCount] = useState(0);
+
+  // Count functions
+  const resetCount = () => setCount(0);
+  const incrementCount = () => setCount((prev) => prev + 1);
+  const decrementCount = () => setCount((prev) => prev - 1);
+
   return (
     <div className="card">
       <Title />
-      <Count />
-      <ResetButton />
-      <CountButtons />
+      <Count count={count} />
+      <ResetButton resetCount={resetCount} />
+      <CountButtons
+        incrementCount={incrementCount}
+        decrementCount={decrementCount}
+      />
     </div>
   );
 }
@@ -15,19 +26,27 @@ const Title = () => {
   return <h1 className="title">Online Counter</h1>;
 };
 
-const Count = () => {
-  return <p className="count">0</p>;
+const Count = ({ count }) => {
+  return <p className="count">{count}</p>;
 };
 
-const ResetButton = () => {
-  return <button className="reset-btn"><ResetIcon className="reset-btn-icon"/></button>;
+const ResetButton = ({ resetCount }) => {
+  return (
+    <button className="reset-btn" onClick={resetCount}>
+      <ResetIcon className="reset-btn-icon" />
+    </button>
+  );
 };
 
-const CountButtons = () => {
+const CountButtons = ({ incrementCount, decrementCount }) => {
   return (
     <div className="button-container">
-      <button className="count-btn"><PlusIcon className="count-btn-icon"/></button>
-      <button className="count-btn"><MinusIcon className="count-btn-icon"/></button>
+      <button className="count-btn" onClick={incrementCount}>
+        <PlusIcon className="count-btn-icon" />
+      </button>
+      <button className="count-btn" onClick={decrementCount}>
+        <MinusIcon className="count-btn-icon" />
+      </button>
     </div>
   );
 };
