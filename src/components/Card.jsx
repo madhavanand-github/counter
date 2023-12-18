c207340430914c9814d018d1351982783090004e
Madhav Anand <madhavanand402@gmail.com>
2023-12-18 13:23:52 +0530

    feat : locked

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function Card() {
   const [count, setCount] = useState(0);
+  const locked = count === 5 ? true : false;
 
   // Count functions
   const resetCount = () => setCount(0);
@@ -10,20 +11,20 @@ export default function Card() {
   const decrementCount = () => setCount((prev) => (prev === 0 ? 0 : prev - 1));
 
   return (
-    <div className="card">
-      <Title />
+    <div className={`card ${locked ? "card--limit" : ""}`}>
+      <Title locked={locked}/>
       <Count count={count} />
       <ResetButton resetCount={resetCount} />
       <div className="button-container">
-        <CountButton handleCount={decrementCount} icon={MinusIcon} />
-        <CountButton handleCount={incrementCount} icon={PlusIcon} />
+        <CountButton handleCount={decrementCount} icon={MinusIcon} locked={locked}/>
+        <CountButton handleCount={incrementCount} icon={PlusIcon} locked={locked} />
       </div>
     </div>
   );
 }
 
-const Title = () => {
-  return <h1 className="title">Online Counter</h1>;
+const Title = ({ locked }) => {
+  return <h1 className="title">{locked ? "LOCKED !! Buy Pro" : "Online Counter"}</h1>;
 };
 
 const Count = ({ count }) => {
@@ -38,9 +39,9 @@ const ResetButton = ({ resetCount }) => {
   );
 };
 
-const CountButton = ({ handleCount, icon: Icon }) => {
+const CountButton = ({ handleCount, icon: Icon, locked }) => {
   return (
-    <button className="count-btn" onClick={handleCount}>
+    <button disabled={locked} className="count-btn" onClick={handleCount}>
       {Icon && <Icon className="count-btn-icon" />}
     </button>
   );
